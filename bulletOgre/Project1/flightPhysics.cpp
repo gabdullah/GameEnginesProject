acc3802f8d2bc66f3a2ffd17d8d4174c1bd38ea0
lunar-mycroft <lunar-mycroft@users.noreply.github.com>
2019-04-15 13:42:27 -0400

    Basic bug fixe for flight physics

--- bulletOgre/Project1/flightPhysics.cpp
+++ bulletOgre/Project1/flightPhysics.cpp
@@ -5,11 +5,11 @@ void FlightPhyics::operator()(float dt, float controls[3], float throttle){
     btVector3 liftDir = getUp();
     btVector3 starboard = flightDir.cross(liftDir);
 
-    float v = mRigidBody->getLinearVelocity().length()
+	float v = mRigidBody->getLinearVelocity().length();
 
     btVector3 lift = liftDir * (mC_l * v * v);
     btVector3 drag = flightDir * (mC_d * v * v) * (-1);
-    btVector3 thrust = flightDir * max_thrust * thottle;
+    btVector3 thrust = flightDir * max_thrust * throttle;
     mRigidBody->applyCentralImpulse((thrust+drag+lift) * dt);
 
     btVector3 pitch = starboard * controlSurfaces[0] * controls[0];
@@ -23,11 +23,11 @@ void FlightPhyics::operator()(float dt, float controls[3], float throttle){
 btVector3 FlightPhyics::getForward(){
     btTransform trans;
     mRigidBody->getMotionState()->getWorldTransform(trans);
-    return trans.inverse()(btVector(1,0,0)); //May need to change this to another unit vector;
+    return trans.inverse()(btVector3(1,0,0)); //May need to change this to another unit vector;
 }
 
 btVector3 FlightPhyics::getUp(){
     btTransform trans;
     mRigidBody->getMotionState()->getWorldTransform(trans);
-    return trans.inverse()(btVector(0,1,0)); //May need to change this to another unit vector;
+    return trans.inverse()(btVector3(0,1,0)); //May need to change this to another unit vector;
 }
--- bulletOgre/Project1/flightPhysics.hpp
+++ bulletOgre/Project1/flightPhysics.hpp
@@ -1,12 +1,12 @@
 #include "btBulletDynamicsCommon.h"
 
 class FlightPhyics{
-    FlightPhyics(btRigidBody * rb, float c_d, float c_d, float thrust): 
+    FlightPhyics(btRigidBody * rb, float c_d, float c_l, float thrust): 
         mRigidBody(rb), 
         mC_d(c_d), 
         mC_l(c_l),
         max_thrust(thrust) {};
-    operator()(float dt, float controls[3], float throttle);
+	void operator()(float dt, float controls[3], float throttle);
 private:
     btVector3 getForward();
     btVector3 getUp();
