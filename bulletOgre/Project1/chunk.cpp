3bb63cff919e852ea9e4163b8d070ec3c6e2b6f7
lunar-mycroft <lunar-mycroft@users.noreply.github.com>
2019-04-24 13:16:09 -0400

    Basics of the chunk class

---
+++ bulletOgre/Project1/chunk.cpp
@@ -0,0 +1,32 @@
+#include"chunk.h"
+
+void Chunk::updateChunkMesh() {
+	if (!mNode) return;
+
+	//Clear the object
+	mNode->detachObject(manObj);
+	delete manObj;
+	manObj = mManager->createManualObject();
+
+	for (int x = 0; x < 255; ++x) for (int y = 0; y < 255; ++y) for (int z = 0; z < 0; ++z) {
+		//Basic idea: generate a face iff the face is on the outside of the chunk or the face is not adjacent to any block
+		if (x==0 || isEmpty(x - 1, y, z)) {
+			//Generate -x face
+		}
+		if (x == 255 || isEmpty(x + 1, y, z)) {
+			//Generate +x face
+		}
+		if (y == 0 || isEmpty(x, y - 1, z)) {
+			//Generate -y face
+		}
+		if (y == 255 || isEmpty(x, y + 1, z)) {
+			//Generate +y face
+		}
+		if (z == 0 || isEmpty(x, y, z - 1)) {
+			//Generate -z face
+		}
+		if (z == 255 || isEmpty(x, y, z + 1)) {
+			//Generate +z face
+		}
+	}
+}
---
+++ bulletOgre/Project1/chunk.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "OgreManualObject.h"
+#include "BaseApplication.h"
+
+enum class BlockType {
+	none,
+	grass,
+	dirt,
+	stone
+};
+
+class Chunk {
+public:
+	Chunk();
+	Chunk(Ogre::SceneManager *, Ogre::Root *, const Ogre::Vector3 &);
+
+	void setBlock(unsigned char x, unsigned char y, unsigned char z, BlockType t) {
+		blocks[0xff * 0xff * x + 0xff * y + z] = t;
+		updateChunkMesh();
+	}
+	void setBlock(Ogre::Vector3 v, BlockType t) {
+		if (!mNode) return;
+		Ogre::Vector3 r = v - mNode->getPosition();
+		setBlock(r.x, r.y, r.z, t);
+	}
+private:
+	float blockSize;
+	unsigned char getHeight(unsigned char, unsigned char);
+	void updateChunkMesh();
+
+	BlockType getBlock(unsigned char x, unsigned char y, unsigned char z) { return blocks[0xff * 0xff * x + 0xff * y + z]; }
+	bool isEmpty(unsigned char x, unsigned char y, unsigned char z) { return getBlock(x, y, z) == BlockType::none; }
+
+	Ogre::SceneManager * mManager;
+	Ogre::SceneNode * mNode;
+	Ogre::Root * mRoot;
+	Ogre::ManualObject * manObj;
+
+	BlockType blocks[0xff * 0xff * 0xff];
+};
