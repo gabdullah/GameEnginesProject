73862e9d9c7d509506737172a1a199273875b72c
lunar-mycroft <lunar-mycroft@users.noreply.github.com>
2019-04-25 20:16:14 -0400

    Bug fix

--- bulletOgre/Project1/chunk.cpp
+++ bulletOgre/Project1/chunk.cpp
@@ -23,7 +23,7 @@ void Chunk::updateChunkMesh() {
 		manObj = mManager->createManualObject();
 	}
 
-	for (int x = 0; x < 255; ++x) for (int y = 0; y < 255; ++y) for (int z = 0; z < 0; ++z) {
+	for (int x = 0; x <= 255; ++x) for (int y = 0; y <= 255; ++y) for (int z = 0; z <= 255; ++z) {
 		//Basic idea: generate a face iff the face is on the outside of the chunk or the face is not adjacent to any block
 		if (x==0 || isEmpty(x - 1, y, z)) {
 			//Generate -x face
